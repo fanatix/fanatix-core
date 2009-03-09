c81d264bc31e6fad6793380af88d9a1feb2b4c66
unknown <sinburn@hotmail.de>
2009-03-09 15:50:45 +0100

    - Added the last scriptname change

--- sql/fixes/utgarde keep/uk.sql
+++ sql/fixes/utgarde keep/uk.sql
@@ -2,3 +2,4 @@ UPDATE `creature_template` SET `ScriptName`='boss_dalronn_the_controller' WHERE 
 UPDATE `creature_template` SET `ScriptName`='boss_ingvar_the_plunderer' WHERE `entry`='23954';
 UPDATE `creature_template` SET `ScriptName`='boss_kelseth' WHERE `entry`='23953';
 UPDATE `creature_template` SET `ScriptName`='boss_skarvald_the_constructor' WHERE `entry`='24200';
+UPDATE `creature_template` SET `ScriptName`='mob_annhylde_the_caller' WHERE entry in ('31655', '24068');
