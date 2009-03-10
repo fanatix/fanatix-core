3daf3300f7ad1baadcc15ecf77e98b5fb385dfb8
unknown <sinburn@hotmail.de>
2009-03-10 19:08:30 +0100

    - You see taht the compiled branch deleted, and i write a tutorial you can by self compile ;) - Different pet fixes

--- src/game/ObjectMgr.cpp
+++ src/game/ObjectMgr.cpp
@@ -5001,7 +5001,7 @@ void ObjectMgr::LoadAreaTriggerTeleports()
         if(at.requiredQuest)
         {
             QuestMap::iterator qReqItr = mQuestTemplates.find(at.requiredQuest);
-            if(qReqItr != mQuestTemplates.end())
+            if(qReqItr == mQuestTemplates.end())
             {
                 sLog.outErrorDb("Required Quest %u not exist for trigger %u, remove quest done requirement.",at.requiredQuest,Trigger_ID);
                 at.requiredQuest = 0;
@@ -5011,7 +5011,7 @@ void ObjectMgr::LoadAreaTriggerTeleports()
         if(at.requiredQuestHeroic)
         {
             QuestMap::iterator qReqItr = mQuestTemplates.find(at.requiredQuestHeroic);
-            if(qReqItr != mQuestTemplates.end())
+            if(qReqItr == mQuestTemplates.end())
             {
                 sLog.outErrorDb("Required Quest %u not exist for trigger %u, remove quest done requirement.",at.requiredQuestHeroic,Trigger_ID);
                 at.requiredQuestHeroic = 0;
