e05cfa4789a2c6f231b74c8f41455a9793352da0
unknown <sinburn@hotmail.de>
2009-03-14 15:22:05 +0100

    - Update to 7454

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -1167,7 +1167,7 @@ void Spell::EffectDummy(uint32 i)
                         return;
 
                     unitTarget->CastSpell(unitTarget, 58419, true);
-                    break;
+                    return;
                 }
                 case 58420:                                 // Portal to Stormwind
                 {
@@ -1175,7 +1175,7 @@ void Spell::EffectDummy(uint32 i)
                         return;
 
                     unitTarget->CastSpell(unitTarget, 58421, true);
-                    break;
+                    return;
                 }
             }
 
@@ -1858,6 +1858,15 @@ void Spell::EffectDummy(uint32 i)
         m_caster->AddPetAura(petSpell);
         return;
     }
+
+    // Script based implementation. Must be used only for not good for implementation in core spell effects
+    // So called only for not proccessed cases
+    if(gameObjTarget)
+        Script->EffectDummyGameObj(m_caster, m_spellInfo->Id, i, gameObjTarget);
+    else if(unitTarget && unitTarget->GetTypeId()==TYPEID_UNIT)
+        Script->EffectDummyCreature(m_caster, m_spellInfo->Id, i, (Creature*)unitTarget);
+    else if(itemTarget)
+        Script->EffectDummyItem(m_caster, m_spellInfo->Id, i, itemTarget);
 }
 
 void Spell::EffectTriggerSpellWithValue(uint32 i)
