ad9cc757fc26ae69fc1d34401a60a11cd96987e8
Thyros <sinburn@hotmail.de>
2009-04-06 01:16:24 +0200

    ^- Update Scriptdev2 to Revision 988

--- src/bindings/ScriptDev2/scripts/zone/coilfang_resevoir/serpent_shrine/boss_leotheras_the_blind.cpp
+++ src/bindings/ScriptDev2/scripts/zone/coilfang_resevoir/serpent_shrine/boss_leotheras_the_blind.cpp
@@ -225,7 +225,7 @@ struct MANGOS_DLL_DECL boss_leotheras_the_blindAI : public ScriptedAI
 
 		InnderDemon_Count = 0;
 
-		m_creature->SetSpeed( MOVE_RUN, 2.0f, true);
+		m_creature->SetSpeed( MOVE_RUN, 1.3f, true);
 
 		m_creature->ApplySpellImmune(0, IMMUNITY_STATE, SPELL_AURA_MOD_TAUNT, true);
 		m_creature->ApplySpellImmune(1, IMMUNITY_EFFECT,SPELL_EFFECT_ATTACK_ME, true);
