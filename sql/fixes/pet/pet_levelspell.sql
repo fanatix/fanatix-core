13cd229c1c173ef88e2bdb3dc986b114631a4d1f
Thyros <sinburn@hotmail.de>
2009-04-09 02:28:48 +0200

    - Added new SQl Files for epts, and moved old update to 0.12 folder

---
+++ sql/fixes/pet/pet_levelspell.sql
@@ -0,0 +1,128 @@
+/*
+MySQL Data Transfer
+Source Host: localhost
+Source Database: mangos
+Target Host: localhost
+Target Database: mangos
+Date: 2009-02-27 ¿ÀÀü 6:44:56
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for pet_levelspell
+-- ----------------------------
+DROP TABLE IF EXISTS `pet_levelspell`;
+CREATE TABLE `pet_levelspell` (
+  `entry` int(11) unsigned NOT NULL default '0',
+  `spell` int(11) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`entry`,`spell`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Pet System';
+
+-- ----------------------------
+-- Records 
+-- ----------------------------
+INSERT INTO `pet_levelspell` VALUES ('15', '19244');
+INSERT INTO `pet_levelspell` VALUES ('15', '19505');
+INSERT INTO `pet_levelspell` VALUES ('15', '19647');
+INSERT INTO `pet_levelspell` VALUES ('15', '19731');
+INSERT INTO `pet_levelspell` VALUES ('15', '19734');
+INSERT INTO `pet_levelspell` VALUES ('15', '19736');
+INSERT INTO `pet_levelspell` VALUES ('15', '27276');
+INSERT INTO `pet_levelspell` VALUES ('15', '27277');
+INSERT INTO `pet_levelspell` VALUES ('15', '48011');
+INSERT INTO `pet_levelspell` VALUES ('15', '54049');
+INSERT INTO `pet_levelspell` VALUES ('15', '54050');
+INSERT INTO `pet_levelspell` VALUES ('15', '54051');
+INSERT INTO `pet_levelspell` VALUES ('15', '54052');
+INSERT INTO `pet_levelspell` VALUES ('15', '54053');
+INSERT INTO `pet_levelspell` VALUES ('15', '54424');
+INSERT INTO `pet_levelspell` VALUES ('15', '57564');
+INSERT INTO `pet_levelspell` VALUES ('15', '57565');
+INSERT INTO `pet_levelspell` VALUES ('15', '57566');
+INSERT INTO `pet_levelspell` VALUES ('15', '57567');
+INSERT INTO `pet_levelspell` VALUES ('16', '3716');
+INSERT INTO `pet_levelspell` VALUES ('16', '7809');
+INSERT INTO `pet_levelspell` VALUES ('16', '7810');
+INSERT INTO `pet_levelspell` VALUES ('16', '7811');
+INSERT INTO `pet_levelspell` VALUES ('16', '7812');
+INSERT INTO `pet_levelspell` VALUES ('16', '11774');
+INSERT INTO `pet_levelspell` VALUES ('16', '11775');
+INSERT INTO `pet_levelspell` VALUES ('16', '17735');
+INSERT INTO `pet_levelspell` VALUES ('16', '17750');
+INSERT INTO `pet_levelspell` VALUES ('16', '17751');
+INSERT INTO `pet_levelspell` VALUES ('16', '17752');
+INSERT INTO `pet_levelspell` VALUES ('16', '17767');
+INSERT INTO `pet_levelspell` VALUES ('16', '17850');
+INSERT INTO `pet_levelspell` VALUES ('16', '17851');
+INSERT INTO `pet_levelspell` VALUES ('16', '17852');
+INSERT INTO `pet_levelspell` VALUES ('16', '17853');
+INSERT INTO `pet_levelspell` VALUES ('16', '17854');
+INSERT INTO `pet_levelspell` VALUES ('16', '19438');
+INSERT INTO `pet_levelspell` VALUES ('16', '19440');
+INSERT INTO `pet_levelspell` VALUES ('16', '19441');
+INSERT INTO `pet_levelspell` VALUES ('16', '19442');
+INSERT INTO `pet_levelspell` VALUES ('16', '19443');
+INSERT INTO `pet_levelspell` VALUES ('16', '27270');
+INSERT INTO `pet_levelspell` VALUES ('16', '27271');
+INSERT INTO `pet_levelspell` VALUES ('16', '27272');
+INSERT INTO `pet_levelspell` VALUES ('16', '27273');
+INSERT INTO `pet_levelspell` VALUES ('16', '33701');
+INSERT INTO `pet_levelspell` VALUES ('16', '47984');
+INSERT INTO `pet_levelspell` VALUES ('16', '47985');
+INSERT INTO `pet_levelspell` VALUES ('16', '47986');
+INSERT INTO `pet_levelspell` VALUES ('16', '47987');
+INSERT INTO `pet_levelspell` VALUES ('16', '47988');
+INSERT INTO `pet_levelspell` VALUES ('16', '47989');
+INSERT INTO `pet_levelspell` VALUES ('16', '47990');
+INSERT INTO `pet_levelspell` VALUES ('17', '6358');
+INSERT INTO `pet_levelspell` VALUES ('17', '6360');
+INSERT INTO `pet_levelspell` VALUES ('17', '7813');
+INSERT INTO `pet_levelspell` VALUES ('17', '7814');
+INSERT INTO `pet_levelspell` VALUES ('17', '7815');
+INSERT INTO `pet_levelspell` VALUES ('17', '7816');
+INSERT INTO `pet_levelspell` VALUES ('17', '7870');
+INSERT INTO `pet_levelspell` VALUES ('17', '11778');
+INSERT INTO `pet_levelspell` VALUES ('17', '11779');
+INSERT INTO `pet_levelspell` VALUES ('17', '11780');
+INSERT INTO `pet_levelspell` VALUES ('17', '11784');
+INSERT INTO `pet_levelspell` VALUES ('17', '11785');
+INSERT INTO `pet_levelspell` VALUES ('17', '27274');
+INSERT INTO `pet_levelspell` VALUES ('17', '27275');
+INSERT INTO `pet_levelspell` VALUES ('17', '47991');
+INSERT INTO `pet_levelspell` VALUES ('17', '47992');
+INSERT INTO `pet_levelspell` VALUES ('23', '2947');
+INSERT INTO `pet_levelspell` VALUES ('23', '3110');
+INSERT INTO `pet_levelspell` VALUES ('23', '4511');
+INSERT INTO `pet_levelspell` VALUES ('23', '6307');
+INSERT INTO `pet_levelspell` VALUES ('23', '7799');
+INSERT INTO `pet_levelspell` VALUES ('23', '7800');
+INSERT INTO `pet_levelspell` VALUES ('23', '7801');
+INSERT INTO `pet_levelspell` VALUES ('23', '7802');
+INSERT INTO `pet_levelspell` VALUES ('23', '7804');
+INSERT INTO `pet_levelspell` VALUES ('23', '7805');
+INSERT INTO `pet_levelspell` VALUES ('23', '8316');
+INSERT INTO `pet_levelspell` VALUES ('23', '8317');
+INSERT INTO `pet_levelspell` VALUES ('23', '11762');
+INSERT INTO `pet_levelspell` VALUES ('23', '11763');
+INSERT INTO `pet_levelspell` VALUES ('23', '11766');
+INSERT INTO `pet_levelspell` VALUES ('23', '11767');
+INSERT INTO `pet_levelspell` VALUES ('23', '11770');
+INSERT INTO `pet_levelspell` VALUES ('23', '11771');
+INSERT INTO `pet_levelspell` VALUES ('23', '27267');
+INSERT INTO `pet_levelspell` VALUES ('23', '27268');
+INSERT INTO `pet_levelspell` VALUES ('23', '27269');
+INSERT INTO `pet_levelspell` VALUES ('23', '47964');
+INSERT INTO `pet_levelspell` VALUES ('23', '47982');
+INSERT INTO `pet_levelspell` VALUES ('23', '47983');
+INSERT INTO `pet_levelspell` VALUES ('29', '30151');
+INSERT INTO `pet_levelspell` VALUES ('29', '30194');
+INSERT INTO `pet_levelspell` VALUES ('29', '30198');
+INSERT INTO `pet_levelspell` VALUES ('29', '30213');
+INSERT INTO `pet_levelspell` VALUES ('29', '30219');
+INSERT INTO `pet_levelspell` VALUES ('29', '30223');
+INSERT INTO `pet_levelspell` VALUES ('29', '33698');
+INSERT INTO `pet_levelspell` VALUES ('29', '33699');
+INSERT INTO `pet_levelspell` VALUES ('29', '33700');
+INSERT INTO `pet_levelspell` VALUES ('29', '47993');
+INSERT INTO `pet_levelspell` VALUES ('29', '47994');
+INSERT INTO `pet_levelspell` VALUES ('29', '47996');
